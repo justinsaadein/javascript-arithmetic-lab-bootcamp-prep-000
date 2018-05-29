0d3d5d92df53d090e0db8288f8a6f8859c1b5e1b
Justin Saadein-Morales <jsaadein@gmail.com>
2018-05-29 00:42:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ function add(a, b, string) {
   string = a + b
   return string
 }
+
+function subtract(a, b, string) {
+  string =  b - a
+  return string
+}
+
+function multiply(a, b, string) {
+  string = a * b
+  return string
+}
+
+function divide(a, b, string) {
+  string = a / b
+  return string
+}
+
+function inc(n) {
+  return n++
+}
